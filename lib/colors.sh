ac4305ca0041342f0dc99b23c1c6ba81f982c7be
Jordan Coin Jackson <jwilliamcj29@gmail.com>
2025-07-05 22:52:12 -0400

    fix: Correct color variable syntax and Bats test flag passing

--- lib/colors.sh
+++ lib/colors.sh
@@ -2,13 +2,16 @@
 
 # ANSI colours (honour NO_COLOR spec)
 if [[ -t 1 && -z "${NO_COLOR:-}" ]]; then
-  B=
-\033[1m'; G=
-\033[32m'; Y=
-\033[33m'; C=
-\033[36m'; R=
-\033[0m'
+  B="\033[1m"
+  G="\033[32m"
+  Y="\033[33m"
+  C="\033[36m"
+  R="\033[0m"
 else
-  B=""; G=""; Y=""; C=""; R=""
+  B=""
+  G=""
+  Y=""
+  C=""
+  R=""
 fi
 export B G Y C R
--- tests/cheatsheet.bats
+++ tests/cheatsheet.bats
@@ -1,7 +1,7 @@
 #!/usr/bin/env bats
 
 @test "cheatsheet --json returns valid json" {
-  run "$GITHUB_WORKSPACE/universal-toolkit.sh" cheatsheet --json
+  run "$GITHUB_WORKSPACE/universal-toolkit.sh" -j cheatsheet
   [ "$status" -eq 0 ]
   echo "$output" | jq -e '.' >/dev/null
 }
